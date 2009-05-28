2c65d6e76d1c22417aec37477f1270b883658aab
Mark Dominus <mjd-www-github.com@plover.com>
2009-05-28 16:26:17 +0000

    Adjust test data to conform to new syntax: quote marks required.

--- treebase-core/src/test/java/org/cipres/treebase/auxdata/SimpleSectionParserTest.java
+++ treebase-core/src/test/java/org/cipres/treebase/auxdata/SimpleSectionParserTest.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 
 public class SimpleSectionParserTest extends TestCase {
 
-	String input = "	>AUTHOR [1]\n		author_id = A415\n		first_name = D.\n		last_name = Schluter\n		email =\n";
+	String input = "	>AUTHOR [1]\n		author_id = 'A415'\n		first_name = 'D.'\n		last_name = 'Schluter'\n		email =\n";
 	SimpleSectionParser ssp = new SimpleSectionParser("AUTHOR");
 	
 	public void testParseString () {
@@ -24,8 +24,8 @@ public class SimpleSectionParserTest extends TestCase {
 	public void testStaticScrewup () {
 		SimpleSectionParser ssp_author = new SimpleSectionParser("AUTHOR");
 		SimpleSectionParser ssp_buthor = new SimpleSectionParser("BUTHOR");
-		String input_author = "	>AUTHOR [1]\n		author_id = A415\n		first_name = D.\n		last_name = Schluter\n		email =\n";
-		String input_buthor = "	>BUTHOR [1]\n		buthor_id = A415\n		first_name = D.\n		last_name = Schluter\n		email =\n";
+		String input_author = "	>AUTHOR [1]\n		author_id = 'A415'\n		first_name = 'D.'\n		last_name = 'Schluter'\n		email =\n";
+		String input_buthor = "	>BUTHOR [1]\n		buthor_id = 'A415'\n		first_name = 'D.'\n		last_name = 'Schluter'\n		email =\n";
 		
 		assertTrue(ssp_author.ParseString(input_author).success());
 		assertTrue(ssp_buthor.ParseString(input_buthor).success());
