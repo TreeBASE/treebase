bc9f6c7208f142285d2dea7c20dbd93b25b29fec
Youjun Guo <youjun.guo@yale.edu>
2009-10-12 23:12:11 +0000

    oai_pmh data_provider

---
+++ data_provider/data_provider_core/src/main/java/org/treebase/oai/App.java
@@ -0,0 +1,13 @@
+package org.treebase.oai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
