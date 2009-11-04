70d77e2482242fb4dd5a0f69479c903241252f91
Rutger Vos <rutgeraldo@gmail.com>
2009-11-04 16:51:46 +0000

    Removed outdated CIPRES/SDSC disclaimers

--- treebase-core/src/test/java/org/cipres/treebase/service/admin/DummyUserDAO.java
+++ treebase-core/src/test/java/org/cipres/treebase/service/admin/DummyUserDAO.java
@@ -1,22 +1,3 @@
-/*
- * Copyright 2005 CIPRES project. http://www.phylo.org/ All Rights Reserved.
- * 
- * Permission to use, copy, modify, and distribute this software and its documentation for
- * educational, research and non-profit purposes, without fee, and without a written agreement is
- * hereby granted, provided that the above copyright notice, this paragraph and the following two
- * paragraphs appear in all copies.
- * 
- * Permission to incorporate this software into commercial products may be obtained by contacting
- * us: http://www.phylo.org/contactUs.html
- * 
- * The software program and documentation are supplied "as is". In no event shall the CIPRES project
- * be liable to any party for direct, indirect, special, incidental, or consequential damages,
- * including lost profits, arising out of the use of this software and its documentation, even if
- * the CIPRES project has been advised of the possibility of such damage. The CIPRES project
- * specifically disclaims any warranties, including, but not limited to, the implied warranties of
- * merchantability and fitness for a particular purpose. The CIPRES project has no obligations to
- * provide maintenance, support, updates, enhancements, or modifications.
- */
 
 package org.cipres.treebase.service.admin;
 
@@ -63,7 +44,7 @@ public class DummyUserDAO extends AbstractDAO implements UserHome {
 			mNewUser.setFirstName("New");
 			mNewUser.setLastName("User");
 			mNewUser.setPassword("dummyPasswd");
-			mNewUser.setEmailAddressString("dummy@sdsc.edu");
+			mNewUser.setEmailAddressString("dummy@treebase.org");
 		}
 		return mNewUser;
 	}
@@ -81,7 +62,7 @@ public class DummyUserDAO extends AbstractDAO implements UserHome {
 			mStoredUser.setFirstName("Dummy");
 			mStoredUser.setLastName("User");
 			mStoredUser.setPassword("dummyPasswd");
-			mStoredUser.setEmailAddressString("dummy@sdsc.edu");
+			mStoredUser.setEmailAddressString("dummy@treebase.org");
 		}
 		return mStoredUser;
 	}
