dcfcf39a2eec9604c3cda75ecf5d3d5a2a4b42f7
Rutger Vos <rutgeraldo@gmail.com>
2010-03-12 23:26:27 +0000

    Removed unused code.

--- treebase-web/src/main/java/org/cipres/treebase/web/controllers/SearchResultsAsRDFController.java
+++ treebase-web/src/main/java/org/cipres/treebase/web/controllers/SearchResultsAsRDFController.java
@@ -32,27 +32,10 @@ public class SearchResultsAsRDFController implements Controller {
 		if ( null != frozenResults ) {
 			String phyloWSPath = request.getSession().getAttribute("originalSection") + "/find";
 			SearchResults<?> searchResults = frozenResults.thaw();
-			request.getSession().setAttribute("searchResultsThawed", searchResults);
-//			SearchResultsType searchResultsType = searchResults.getResultType();
-//			switch(searchResultsType) {
-//				case NONE   : phyloWSPath = ""; break;
-//				case STUDY  : phyloWSPath = "study/find"; break;
-//				case MATRIX : phyloWSPath = "matrix/find"; break;
-//				case TREE   : phyloWSPath = "tree/find"; break;
-//				case TAXON  : phyloWSPath = "taxon/find"; break;
-//			}
-			
-			StringBuffer domainAddress = new StringBuffer("http://");
-			domainAddress
-				.append(request.getServerName())
-				.append(':')
-				.append(request.getServerPort());
-			StringBuffer baseURL = new StringBuffer(domainAddress);
-			baseURL.append("/treebase-web/phylows");
+			request.getSession().setAttribute("searchResultsThawed", searchResults);			
 			request.getSession().setAttribute("recordSchema", request.getSession().getAttribute("recordSchema"));
 			request.getSession().setAttribute("format", "rss1");
-			request.getSession().setAttribute("baseURL", baseURL.toString());
-			request.getSession().setAttribute("domainAddress", domainAddress.toString());
+			request.getSession().setAttribute("baseURL", TreebaseUtil.getPurlDomain());
 			request.getSession().setAttribute("phyloWSPath", phyloWSPath);
 			if ( null != request.getParameter("query") ) {			
 				String query = request.getParameter("query");						
@@ -64,52 +47,6 @@ public class SearchResultsAsRDFController implements Controller {
 			}
 		}
 		return new ModelAndView("searchResultsAsRDF");
-
-		
-		/*
-		
-		NamespacedGUID namespacedGUID = new NamespacedGUID(request.getParameter("namespacedGUID"));
-		TreebaseIDString treebaseIDString = null;
-		PhyloWSPath phyloWSPath = null;
-		request.setAttribute("hasWebPage", false);
-		request.setAttribute("hasNexus", false);
-		request.setAttribute("hasNeXML", false);
-		request.setAttribute("hasRdf", false);
-		try {
-			treebaseIDString = namespacedGUID.getTreebaseIDString();
-		} catch ( Exception e ) {
-			e.printStackTrace();
-		}
-		if ( null != treebaseIDString ) {
-			if ( null != treebaseIDString.getTypePrefix() ) {
-				Class<?> theClass = TreebaseIDString.getClassForPrefix(treebaseIDString.getTypePrefix());
-				if ( null != theClass ) {
-					if ( theClass.isAssignableFrom(PhyloTree.class) 
-							|| theClass.isAssignableFrom(Matrix.class) 
-							|| theClass.isAssignableFrom(Study.class) ) {
-						request.setAttribute("hasWebPage", true);
-						request.setAttribute("hasNexus", true);
-						request.setAttribute("hasNeXML", true);
-						request.setAttribute("hasRdf", true);
-					}
-					phyloWSPath = new PhyloWSPath(theClass.getPackage(),namespacedGUID);
-				}
-			}
-		}
-		request.getSession().setAttribute("namespacedGUID", namespacedGUID.toString());
-		// <c:set var="baseURL" value="http://localhost:8080/treebase-web/PhyloWS"/>
-		StringBuffer domainAddress = new StringBuffer("http://");
-		domainAddress
-			.append(request.getServerName())
-			.append(':')
-			.append(request.getServerPort());
-		StringBuffer baseURL = new StringBuffer(domainAddress);
-		baseURL.append("/treebase-web/phylows");
-		request.getSession().setAttribute("baseURL", baseURL.toString());
-		request.getSession().setAttribute("domainAddress", domainAddress.toString());
-		request.getSession().setAttribute("phyloWSPath", phyloWSPath);
-		return new ModelAndView("anyObjectAsRDF");
-		*/
 	}
 
 }
