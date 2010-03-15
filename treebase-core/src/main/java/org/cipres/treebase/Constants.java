f7d158d0161245584c556123d4730f6a6e49c534
Rutger Vos <rutgeraldo@gmail.com>
2010-03-15 01:32:50 +0000

    Now stores the URIs for Annotation predicates

--- treebase-core/src/main/java/org/cipres/treebase/Constants.java
+++ treebase-core/src/main/java/org/cipres/treebase/Constants.java
@@ -1,7 +1,16 @@
 
 package org.cipres.treebase;
 
+import java.net.URI;
+
 public class Constants {
 	public static final int MIN_INTERESTING_PHYLOCHAR_ID = 7;
+	public static final URI DwCURI = URI.create("http://rs.tdwg.org/dwc/terms/");
+	public static final URI TBTermsURI = URI.create("http://treebase.org/terms#");
+	public static final URI DCTermsURI = URI.create("http://purl.org/dc/terms/");
+	public static final URI DCURI = URI.create("http://purl.org/dc/elements/1.1/");
+	public static final URI PrismURI = URI.create("http://prismstandard.org/namespaces/1.2/basic/");
+	public static final URI BaseURI = URI.create("http://purl.org/PHYLO/TREEBASE/PHYLOWS/");
+	public static final URI OWLURI = URI.create("http://www.w3.org/2002/07/owl#");	
 }
 
