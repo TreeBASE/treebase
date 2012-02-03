3bed5bb5eb01bcfe4402bd0cc536725e60035d53
Rutger Vos <rutgeraldo@gmail.com>
2012-02-03 22:45:53 +0000

    We're now setting the purl base from what's supplied by TreebaseUtil.getPurlBase, no longer using the ugly NCBI URLs but the UniProt ones.

--- treebase-core/src/main/java/org/cipres/treebase/Constants.java
+++ treebase-core/src/main/java/org/cipres/treebase/Constants.java
@@ -10,12 +10,11 @@ public class Constants {
 	public static final URI DCTermsURI = URI.create("http://purl.org/dc/terms/");
 	public static final URI DCURI = URI.create("http://purl.org/dc/elements/1.1/");
 	public static final URI PrismURI = URI.create("http://prismstandard.org/namespaces/1.2/basic/");
-	public static final URI BaseURI = URI.create("http://purl.org/phylo/treebase/phylows/");
+	public static final URI BaseURI = URI.create(TreebaseUtil.getPurlBase());
 	public static final URI OWLURI = URI.create("http://www.w3.org/2002/07/owl#");	
 	public static final URI SKOSURI = URI.create("http://www.w3.org/2004/02/skos/core#");
 	public static final URI RDFSURI = URI.create("http://www.w3.org/2000/01/rdf-schema#");
 	public static final URI RDFURI = URI.create("http://www.w3.org/1999/02/22-rdf-syntax-ns#");
-	public static final String NCBITaxonomyBase = "http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=";
 	public static final String uBioBase = "http://www.ubio.org/authority/metadata.php?lsid=urn:lsid:ubio.org:namebank:";
 	public static final String NCBITaxonomyFormat = "http://purl.uniprot.org/taxonomy/%s";
 }
