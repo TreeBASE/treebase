4392d171c15f15c97278a77d1b2166f99fc75c74
Rutger Vos <rutgeraldo@gmail.com>
2012-02-04 00:23:33 +0000

    now uses same purl as in owl file for treebase terms

--- treebase-core/src/main/java/org/cipres/treebase/Constants.java
+++ treebase-core/src/main/java/org/cipres/treebase/Constants.java
@@ -6,7 +6,7 @@ import java.net.URI;
 public class Constants {
 	public static final int MIN_INTERESTING_PHYLOCHAR_ID = 7;
 	public static final URI DwCURI = URI.create("http://rs.tdwg.org/dwc/terms/");
-	public static final URI TBTermsURI = URI.create("http://treebase.org/terms#");
+	public static final URI TBTermsURI = URI.create("http://purl.org/phylo/treebase/2.0/terms#");
 	public static final URI DCTermsURI = URI.create("http://purl.org/dc/terms/");
 	public static final URI DCURI = URI.create("http://purl.org/dc/elements/1.1/");
 	public static final URI PrismURI = URI.create("http://prismstandard.org/namespaces/1.2/basic/");
